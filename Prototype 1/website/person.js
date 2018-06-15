f9a51896bba1daebfa32fc67453306f748639947
mpafit02 <mpafit02@cs.ucy.ac.cy>
2018-06-15 13:52:13 +0300

    Working $ref

--- Prototype 1/website/person.js
+++ Prototype 1/website/person.js
@@ -8,9 +8,7 @@
 //   console.log('User data is INVALID!');
 //   console.log(validate.errors);
 // }
-
-
-
+var json;
 $(function () {
     data = $.ajax({
         url: "schema.json",
@@ -18,38 +16,44 @@ $(function () {
         type: "get",
         cache: false,
         success: function (data) {
+            json = data;
             document.getElementById("app").innerHTML =
-                `         
-                <main role="main" style = "margin: 20px">
-                <br>
-                <form><legend>Form</legend><fieldset>
-                ${create(data)} 
-                </fieldset></form>
+                `
+                <main role="main" style = "margin: 24px">
+                <form><legend>Form</legend>
+                ${create(data.properties)} 
+                </form>
                 </main>   
-                
                 `;
 
-            function create(data) {
-                var path = data.properties;
+            function create(link) {
                 var hasReference = false;
                 var components = "";
                 var layout = "";
-                var key = Object.keys(data);
-                var val = Object.values(data);
+                var key = Object.keys(link);
+                var val = Object.values(link);
                 for (i in key) {
                     if (typeof val[i] === "object" && key[i] != 'items') {
                         hasReference = false;
-                        layout += "<div class='border border-danger rounded form-group row' style = 'margin: 12px'><label class='my-1' style ='padding: 12px'>" + key[i] + "</label>" + create(val[i]);
+                        layout += "<p><div class='border border-danger rounded' style = 'padding: 12px'><label >" + key[i] + "</label>" + create(val[i]);
                     } else if (key[i] === "type" && val[i] === "array") {
-    //path = .... // create(path); **********************************************
-                        console.log(data.items.$ref);
+                        var path = link.items.$ref;
+                        var p = json;
+                        if (typeof path != "undefined") {
+                            var keywords = path.substring(2, path.length).split("/");
+                            for (i in keywords) {
+                                p = p[keywords[i]];
+                            }
+                            console.log(p);
+                            layout += create(p);
+                        }
                         hasReference = true;
                         layout += "</div></div>";
                     } else {
                         if (val[i] === "boolean") {
                             components += "<label class='form-check-label'><input type = 'checkBox'></label>";
                         } else {
-                            components += "<div class='col-sm-5 col-form-label'><input class='form-control form-control' id='colFormLabelSm' placeholder = '" + val[i] + "'></div></div>";
+                            components += "<div class='col-form-label'><input class='form-control' style = 'width: 40%' placeholder = '" + val[i] + "'></div></div></p>";
                         }
                     }
                 }
@@ -60,21 +64,73 @@ $(function () {
                 }
                 return layout;
             }
-
-            function displayKeys(data) {
-                var keys = data;
-                var out = "<div class='bd-sidebar' style = 'position: fixed; top: 0; left: 0; margin: 30px;'><nav><div class = 'btn-group-vertical' align = 'center'>";
-                for (i in keys) {
-                    out += "<button type='button' class='btn btn-danger btn-lg'>" + keys[i] + "</button>";
-                }
-                out += "</div></nav></div>"
-                return out;
-            }
-
         }
     });
 });
 
+// $(function () {
+//     data = $.ajax({
+//         url: "schema.json",
+//         dataType: "json",
+//         type: "get",
+//         cache: false,
+//         success: function (data) {
+//             document.getElementById("app").innerHTML =
+//                 `         
+//                 <main role="main" style = "margin: 20px">
+//                 <br>
+//                 <form><legend>Form</legend><fieldset>
+//                 ${create(data)} 
+//                 </fieldset></form>
+//                 </main>   
+
+//                 `;
+
+//             function create(data) {
+//                 var path = data.properties;
+//                 var hasReference = false;
+//                 var components = "";
+//                 var layout = "";
+//                 var key = Object.keys(data);
+//                 var val = Object.values(data);
+//                 for (i in key) {
+//                     if (typeof val[i] === "object" && key[i] != 'items') {
+//                         hasReference = false;
+//                         layout += "<div class='border border-danger rounded form-group row' style = 'margin: 12px'><label class='my-1' style ='padding: 12px'>" + key[i] + "</label>" + create(val[i]);
+//                     } else if (key[i] === "type" && val[i] === "array") {
+//     //path = .... // create(path); **********************************************
+//                         console.log(data.items.$ref);
+//                         hasReference = true;
+//                         layout += "</div></div>";
+//                     } else {
+//                         if (val[i] === "boolean") {
+//                             components += "<label class='form-check-label'><input type = 'checkBox'></label>";
+//                         } else {
+//                             components += "<div class='col-sm-5 col-form-label'><input class='form-control form-control' id='colFormLabelSm' placeholder = '" + val[i] + "'></div></div>";
+//                         }
+//                     }
+//                 }
+//                 if (!hasReference) {
+//                     layout += components;
+//                 } else {
+//                     //....
+//                 }
+//                 return layout;
+//             }
+
+//             function displayKeys(data) {
+//                 var keys = data;
+//                 var out = "<div class='bd-sidebar' style = 'position: fixed; top: 0; left: 0; margin: 30px;'><nav><div class = 'btn-group-vertical' align = 'center'>";
+//                 for (i in keys) {
+//                     out += "<button type='button' class='btn btn-danger btn-lg'>" + keys[i] + "</button>";
+//                 }
+//                 out += "</div></nav></div>"
+//                 return out;
+//             }
+
+//         }
+//     });
+// });
 
 
 
