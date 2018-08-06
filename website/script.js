af02f032df8fa0342320fd7d80edbdcb3a503aed
mpafit02 <mpafit02@cs.ucy.ac.cy>
2018-08-06 23:05:49 +0300

    Create multiple items, Load json data not fully working

--- website/script.js
+++ website/script.js
@@ -8,7 +8,6 @@ var isPercentage = false;
 var objectName = "";
 var returnContent = "";
 var modalMap = new Map();
-var itemMap = new Map();
 var jsonDataMap = new Map();
 var minItems = Number.MIN_VALUE;
 var minProperties = Number.MIN_VALUE;
@@ -26,7 +25,24 @@ var itemStack = [];
 var outputData = {};
 var formContent = {};
 var dataJSON = {};
+var itemsCounter = 1;
 var zIndex;
+
+// Function for Creating the Upload json schema card
+function createStartCard() {
+    var layout = "";
+    layout += "<div id='upload-card' class='card text-white centered-card animated bounceInDown'>";
+    layout += "<div class='card-header'>";
+    layout += "<h5 class='card-title'>Upload JSON Schema</h5>";
+    layout += "</div>";
+    layout += "<div class='card-body'>";
+    layout += "<div class='custom-file'>";
+    layout += "<input type='file' class='custom-file-input' id='inputFile' onchange='uploadSchemaFile(event)'>";
+    layout += "<label id='input-schema-label' class='custom-file-label' for='inputGroupFile01'>Choose JSON file</label></div>";
+    layout += "<p id='output'></p></div><div>";
+    $('#start-screen').html(layout);
+    $('#start-btn').fadeOut();
+}
 // -----------------------------------JSON upload---------------------------------------
 // Function for Upload json file
 function uploadJsonFile(event) {
@@ -45,12 +61,10 @@ function uploadJsonFile(event) {
             jsonData = JSON.parse(file);
             // Check if this object is loaded
             findPath(jsonData);
-            console.log(jsonData);
             $("#outputJsonData").html(txt);
             document.getElementById("upload-json-btn").addEventListener("click", function () {
                 $("#upload-json-card").fadeOut();
-                $('#load-btn').show();
-                $('#start-new-btn').show();
+                $("#load-btn").fadeOut();
             });
         } else {
             txt += "<div style='color:#ff2045;'>";
@@ -118,25 +132,82 @@ function findPath(data) {
 // Main function that creates the html
 function createPage() {
     document.getElementById("app").innerHTML = `
-        <nav class="navbar fixed-top top-nav text-light container-fluid" style='background-color: rgba(30,30,30,0.6);'>
-            <div class="container-fluid">
-                <div class='navbar-header'>   
-                    <a class="navbar-brand mb-0 h1 text-light" href="index.html">${json.description}</a>
-                </div>
-                <input type="button" class='btn btn-primary btn-sm float-right' id="download-btn" value='Download' disabled>
+        <nav class="navbar fixed-top top-nav text-light container-fluid">
+            <div class='navbar-header'>   
+                <a class="navbar-brand text-light" href="index.html" style="font-size:22px">${json.description}</a>
+            </div>
+            <input type="button" class='btn btn-primary btn-sm float-right' id="download-btn" value='Download' disabled>
+        </nav>
+        <nav class="side-nav text-light">
+            <div id="existing-items">
+                <input id='start-new-btn' type='button' class='btn btn-primary btn-sm btn-block' value='+add new Property'>
+                <input id='load-btn' type='button' class='btn btn-dark btn-sm btn-block' value='Load Properties'>    
             </div>
         </nav>
-        <div class="wrapper">
-            <input id='load-btn' type='button' class='btn btn-dark' value='Load JSON'>
-            <input id='start-new-btn' type='button' class='btn btn-primary' data-toggle='modal' data-target='#base-modal' value='Create New'>
-        </div>
         <div id='upload-modal-html'></div>
         <div id='base-modal-html' style='z-index:1000;'></div>
         <div id='set-modal-html'></div>
         <div id='select-modal-html'></div>
+        <div id='confirmation-modal-html'></div>
     `;
-    // Create the base modal
-    baseModalCreation("Properties", json.properties);
+    document.getElementById('start-new-btn').addEventListener('click', function () {
+        var modalId = "Property-" + itemsCounter + "-modal";
+        var finishBtn = "finish-" + itemsCounter + "-btn";
+        var saveBtn = "save-" + itemsCounter + "-btn";
+        var cancelBtn = "cancel-" + itemsCounter + "-btn";
+        // Create the base modal
+        baseModalCreation();
+        // Presents the modal
+        modalShow(modalId);
+        // Finish form button
+        document.getElementById(finishBtn).addEventListener("click", function () {
+            swal({
+                type: 'success',
+                title: 'Your work has been saved',
+                showConfirmButton: false,
+                timer: 1500
+            });
+            var id = "Property-" + itemsCounter;
+            var editBtn = id + '-edit-btn';
+            var btnGroup = id + '-btn-group';
+            var deleteBtn = id + '-delete-btn';
+            // Create buttons for edit dialog
+            var layout = "<div id='" + btnGroup + "' class='btn-group btn-line' role='group' style='width: 100%'>";
+            layout += "<button type='button' id='" + editBtn + "' class='btn btn-primary btn-sm' data-toggle='modal' data-target='#base-modal' style='width: 80%;'>Property " + itemsCounter + "</button>";
+            layout += "<button type='button' id='" + deleteBtn + "' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#confirmation-modal' style='width: 20%;'>";
+            layout += "<i class='fa fa-trash-o' style='font-size:18px' aria-hidden='true'></i></button></div>";
+            $("#existing-items").prepend(layout);
+            itemsCounter++;
+            document.getElementById(editBtn).addEventListener("click", function () {
+                // Presents the modal
+                modalShow(modalId);
+            });
+            document.getElementById(deleteBtn).addEventListener("click", function () {
+                document.getElementById('delete-item-btn').addEventListener("click", function () {
+                    formContent[id] = {};
+                    $("#" + btnGroup).hide();
+                    $('#confirmation-modal').modal('hide');
+                });
+            });
+            $("#" + modalId).modal('hide');
+            $('#' + finishBtn).hide();
+            $('#' + cancelBtn).hide();
+            $('#' + saveBtn).removeAttr('hidden');
+            $("#download-btn").removeAttr("disabled");
+        });
+        // Save form button
+        document.getElementById(saveBtn).addEventListener("click", function () {
+            swal({
+                type: 'success',
+                title: 'Your work has been saved',
+                showConfirmButton: false,
+                timer: 1500
+            });
+            $("#" + modalId).modal('hide');
+        });
+    });
+    // Create the confirmation modal
+    confirmationModalCreation();
     // Call function for listeners
     callListener();
     // ----------------------------------------------------Methods--------------------------------------------------------
@@ -160,8 +231,6 @@ function createPage() {
     document.getElementById('load-btn').addEventListener('click', function () {
         // Create the upload json modal
         uploadJsonModalCreation();
-        $('#load-btn').hide();
-        $('#start-new-btn').hide();
     });
     // Control depth of modal backdrop
     $(document).on('show.bs.modal', '.modal', function () {
@@ -224,17 +293,6 @@ function createPage() {
         element.click();
         document.body.removeChild(element);
     }
-    // Save form button
-    document.getElementById('save-form-btn').addEventListener("click", function () {
-        swal({
-            type: 'success',
-            title: 'Your work has been saved',
-            showConfirmButton: false,
-            timer: 1500
-        });
-        $("#base-modal").modal('hide');
-        $("#download-btn").removeAttr("disabled");
-    });
     // Call Listeners
     function callListener() {
         // Check for key press actions in set modal
@@ -537,25 +595,27 @@ function createPage() {
         $("#upload-modal-html").html(layout);
     }
     // Base Modal
-    function baseModalCreation(id, path) {
+    function baseModalCreation() {
+        var path = json.properties;
         var layout = "";
         // Modal
-        layout += "<div class='modal fade' id='base-modal' role='dialog'>";
+        layout += "<div class='modal fade' id='Property-" + itemsCounter + "-modal' role='dialog'>";
         layout += "<div class='modal-dialog modal-lg'>";
         layout += "<div class='modal-content'>";
         // Header
         layout += "<div class='modal-header'>";
-        layout += "<h5 class='modal-title'>" + id + "</h5></div>";
+        layout += "<h5 class='modal-title'>Property " + itemsCounter + "</h5></div>";
         // Body 
-        var formid = "base-form";
+        var formid = "base-" + itemsCounter + "-form";
         layout += "<form id='" + formid + "' class='needs-validation' novalidate>";
         layout += "<div id='base-modal-body' class='modal-body'>" + create(path, formid) + "</div>";
         // Footer
         layout += "<div class='modal-footer'>";
-        layout += "<input type='button' class='btn btn-secondary' data-dismiss='modal' value='Cancel'>";
-        layout += "<input type='button' class='btn btn-primary' id='save-form-btn' form='" + formid + "' value='Finish'>";
+        layout += "<input type='button' class='btn btn-secondary' id='cancel-" + itemsCounter + "-btn' data-dismiss='modal' value='Cancel'>";
+        layout += "<input type='button' class='btn btn-primary' id='finish-" + itemsCounter + "-btn' form='" + formid + "' value='Finish'>";
+        layout += "<input type='button' class='btn btn-success' id='save-" + itemsCounter + "-btn' form='" + formid + "' value='Save' hidden>";
         layout += "</div></form></div></div></div>";
-        $("#base-modal-html").html(layout);
+        document.getElementById("base-modal-html").innerHTML += layout;
     }
     // Set Modal
     function setModalCreation(id, path) {
@@ -631,6 +691,26 @@ function createPage() {
         layout += "<input type='button' id='" + id + "-create-btn' class='btn btn-primary' value='Create " + upperCaseFirst(id) + "'>";
         layout += "</div></div></div></div>";
         document.getElementById("select-modal-html").innerHTML += layout;
+    }
+    // Confirmation Modal
+    function confirmationModalCreation() {
+        var layout = "";
+        // Modal
+        layout += "<div class='modal fade centered' id='confirmation-modal' role='dialog' style='z-index:4000'>";
+        layout += "<div class='modal-dialog modal-sm'>";
+        layout += "<div class='modal-content'>";
+        // Header
+        layout += "<div class='modal-header bg-danger'></div>";
+        // Body 
+        layout += "<div class='modal-body'>";
+        layout += "<h5 class='modal-title'><i class='fa fa-trash-o' style='font-size:18px' aria-hidden='true'></i> Delete</h5>";
+        layout += "<br>Are you sure you want to delete this item?</div>";
+        // Footer
+        layout += "<div class='modal-footer'>";
+        layout += "<input type='button' class='btn btn-secondary' data-dismiss='modal' value='Cancel'>";
+        layout += "<input type='submit' class='btn btn-danger' id='delete-item-btn' value='Delete'>";
+        layout += "</div></div></div></div>";
+        $('#confirmation-modal-html').html(layout);
     }
     //-------------------------------Cases--------------------------------
     // Input Case
